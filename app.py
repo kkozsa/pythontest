3db64d5398eaaca6e157ab978332dad70f48c9eb
kkozsa <68240122+kkozsa@users.noreply.github.com>
2024-04-04 20:44:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,61 @@
 from flask import Flask
+from flask import render_template
 from flask import request
-from flask_mysqldb import MySQL
+import mysql.connector
 from flask_cors import CORS
 import json
-mysql = MySQL()
+mysql = mysql.connector.connect(user='web', password='webPass',
+  host='127.0.0.1',
+  database='student')
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 app = Flask(__name__)
 CORS(app)
 # My SQL Instance configurations
 # Change the HOST IP and Password to match your instance configurations
-app.config['MYSQL_USER'] = 'web'
-app.config['MYSQL_PASSWORD'] = 'webPass'
-app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'localhost' #for now
-mysql.init_app(app)
 
-@app.route("/add") #Add Student
+@app.route("/test")#URL leading to method
+def test(): # Name of the method
+ return("Hello World!<BR/>THIS IS ANOTHER TEST!") #indent this line
+
+@app.route("/yest")#URL leading to method
+def yest(): # Name of the method
+ return("Hello World!<BR/>THIS IS YET ANOTHER TEST!") #indent this line
+
+@app.route("/add", methods=['GET', 'POST']) #Add Student
 def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email)
-  cur.execute(s)
-  mysql.connection.commit()
+  if request.method == 'POST':
+    name = request.form['name']
+    email = request.form['email']
+    print(name,email)
+    cur = mysql.cursor() #create a connection to the SQL instance
+    s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email)
+    app.logger.info(s)
+    cur.execute(s)
+    mysql.commit()
+  else:
+    return render_template('add.html')
 
   return '{"Result":"Success"}'
-@app.route("/delete") #Default - Show Data
-def delete(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  #cur.execute('''SELECT * FROM students''') # execute an SQL statment
-  response={'Results':'Error','Message':'Not Implemented Yet!'}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=404,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
-
 @app.route("/") #Default - Show Data
 def hello(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  cur = mysql.cursor() #create a connection to the SQL instance
   cur.execute('''SELECT * FROM students''') # execute an SQL statment
   rv = cur.fetchall() #Retreive all rows returend by the SQL statment
   Results=[]
