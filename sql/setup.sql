6bbdf262319b2c82531d23ed58cc2f1d02581361
kkozsa <68240122+kkozsa@users.noreply.github.com>
2024-04-08 19:03:28 +0100

    Update setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,1 +1,9 @@
+CREATE DATABASE student; 
+USE student;
+CREATE TABLE students (studentName VARCHAR(255), email VARCHAR(255), studentID INT NOT NULL AUTO_INCREMENT,
+PRIMARY KEY(studentID));
 
+INSERT INTO students (studentName, email) values ("first student", "firststudent@mydbs.ie"); 
+INSERT INTO students (studentName, email) values ("second student", "secondstudent@mydbs.ie ");
+SELECT * FROM students;
+exit;
